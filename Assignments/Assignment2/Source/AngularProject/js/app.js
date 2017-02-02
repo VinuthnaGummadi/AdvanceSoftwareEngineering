bfabe5f30b24862bdeb63913f3227c257399d151
VinuthnaGummadi <vinuthna.gummadi@gmail.com>
2017-02-01 23:06:02 -0600

    Assignment 2

---
+++ Assignments/Assignment2/Source/AngularProject/js/app.js
@@ -0,0 +1,35 @@
+/**
+ * Created by vinuthna on 01-02-2017.
+ */
+
+
+
+var module = angular.module("myApp",[]);
+module.controller("MainController",Main);
+module.controller("LoginController",Login);
+module.controller("RegisterController",Register);
+
+function Main($scope,$window) {
+
+    $scope.loginBtn = function () {
+        $window.location.href = 'login.html';
+    }
+    $scope.registerBtn = function () {
+        $window.location.href = 'register.html';
+    }
+}
+
+function Login($scope,$window){
+
+    $scope.login = function () {
+        $window.location.href = 'home.html';
+    }
+}
+
+function Register($scope,$window){
+
+    $scope.register = function () {
+        $window.location.href = 'index.html';
+    }
+}
+
